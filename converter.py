14912deb1edf6385813a1ef88d602e2898b41321
Thomas <92346180+thomaspradae@users.noreply.github.com>
2024-06-03 00:42:44 -0500

    v.2 workflow testing

--- converter.py
+++ converter.py
@@ -1,2 +1,34 @@
 import re
 import os 
+
+def convert_file(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        content = file.read()
+
+    # No conversion zones
+    content = re.sub(r'<!-- noconvert -->(.*?)<!-- /noconvert -->', lambda x: x.group(0), content, flags=re.DOTALL)
+
+    # LaTeX conversions
+    content = re.sub(r'\$\$(.+?)\$\$', r'\\[ \1 \\]', content)
+    content = re.sub(r'\$(.+?)\$', r'\\( \1 \\)', content)
+
+    with open(file_path, 'w', encoding='utf-8') as file:
+        file.write(content)
+
+
+def main():
+
+    base_path = os.getcwd()
+
+    folders = ['building', 'notes', 'writing']
+
+    for folder in folders:
+        full_path = os.path.join(base_path, folder)
+
+        for root, dirs, files in os.walk(full_path):
+            for file in files:
+                if file.endswith('.md'):
+                    convert_file(os.path.join(root, file))
+                    
+if __name__ == '__main__':
+    main()
--- writing/2024-02-27-minimum-viable-study.md
+++ writing/2024-02-27-minimum-viable-study.md
@@ -9,6 +9,18 @@ excerpt: A huge thanks to Andrej Karpathy and MJ. If there's some class you real
 
 ---
 
+$$ e^{something} $$
+Hi, my name is thomas, that is spelled $10 = 10 + 0$
+
+Let's add some more latex here:
+
+$$ e^{i\pi} + 1 = 0 $$
+$$ e^{i\pi} + 1 = 0 $$
+
+Now some inline latex with the dollar sign:
+
+hi $e^{i\pi} + 1 = 0$ and $e^{i\pi} + 1 = 0$
+
 A hugeeee thanks to Andrej Karpathy and MJ. 
 
 If there's some class you really don't like[^1], and you recognize that the opportunity cost is too high (to the point where your head hurts each time you think about wasting just another second stuck in that chair[^2] , set apart some time that counts as a minimum viable study session. Set it so that you aim for a grade you're okay with, and once you hit your quota, let the struggle go, and focus on your shit. 
